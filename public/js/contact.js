3a2acd7eaef7a0417ec9dcd727da6bcbee253da8
Nobru44 <39130278+Nobru44@users.noreply.github.com>
2018-11-01 10:58:41 +0100

    Add files via upload

--- public/js/contact.js
+++ public/js/contact.js
@@ -7,8 +7,6 @@
 var firstname = document.querySelector("#firstname");
 firstname.addEventListener("keyup", verifFirst); 
 
-var lastname = document.querySelector("#message");
-lastname.addEventListener("keyup", verifMessage); 
 
 $("#mail").on('keyup', verifMailBis);
 
@@ -19,11 +17,6 @@ $(".btn-submit").on('click', function(event) {
     if(!regexNom.test($("#firstname").val())) {
       $("#aidefirstname").html("Saisie invalide");
       $("#aidefirstname").css('color', 'red');
-      event.preventDefault();
-    } 
-    if(!regexNom.test($("#message").val())) {
-      $("#aideMessage").html("Saisie invalide");
-      $("#aideMessage").css('color', 'red');
       event.preventDefault();
     }
     verifMailBis(event);
